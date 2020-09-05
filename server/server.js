d35d4eb7bc129933ed043693f67ee759443b66c7
Karl Watkins <Watkins_Karl@yahoo.com>
2020-09-05 10:48:42 -0600

    server.js changes

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+   res.sendFile(path.join(public, 'index.html'));
 });
 
 app.listen(port, () => {
