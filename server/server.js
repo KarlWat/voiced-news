ea8b8f5250463cd33cc4ddfab20bd374d38a47a6
Karl Watkins <Watkins_Karl@yahoo.com>
2020-09-05 14:17:20 -0600

    Try

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
+const url = new URL(document.currentScript.src);
+const widgetLink = url.href.substring(0, url.href.lastIndexOf('/') + 1);
+__webpack_public_path__ = widgetLink;
 const port = process.env.PORT || 3000;
 
 
-app.use(express.static(__dirname + '/public/build'));
+app.use(express.static(__dirname + '/publicPath/build'));
 
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
