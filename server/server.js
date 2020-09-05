3b9ea477c5e678bf0a23dbaea5f890c9ce6b1d43
Karl Watkins <Watkins_Karl@yahoo.com>
2020-09-05 14:19:34 -0600

    server

--- server/server.js
+++ server/server.js
@@ -2,15 +2,15 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-const url = new URL(document.currentScript.src);
-const widgetLink = url.href.substring(0, url.href.lastIndexOf('/') + 1);
-__webpack_public_path__ = widgetLink;
+
 const port = process.env.PORT || 3000;
 
 
 app.use(express.static(__dirname + '/publicPath/build'));
 
-
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath));
+});
 
 app.listen(port, () => {
    console.log('Server is up!');
