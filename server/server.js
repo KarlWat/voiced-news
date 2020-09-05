5cdd41ae9ac597e47920a5d562105f821a4f7027
Karl Watkins <Watkins_Karl@yahoo.com>
2020-09-04 21:02:43 -0600

    manifest.js update

--- server/server.js
+++ server/server.js
@@ -4,12 +4,11 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path_join(__dirname, '../client/build'))
-)
+app.use(express.static(publicPath));
+
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
-
 
 app.listen(port, () => {
    console.log('Server is up!');
